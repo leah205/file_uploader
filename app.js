36fc68a2f4dab5ad9523bf6648bd60d91f838eb1
leah205 <leahtiktin@gmail.com>
2025-08-12 17:19:42 -0600

    feat: setup session and passport authentiation logic

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
 const path = require("node:path");
-const { Pool } = require("pg");
 const express = require("express");
 const session = require("express-session");
 const passport = require("passport");
-const LocalStrategy = require('passport-local').Strategy;
+const passport = require("./passport")
+const flash = require('connect-flash')
+const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
+const { PrismaClient } = require('@prisma/client');
 
 
 const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(session({ secret: process.env.SECRET, resave: false, saveUninitialized: false }));
+
+app.use(
+  expressSession({
+    cookie: {
+     maxAge: 7 * 24 * 60 * 60 * 1000 // ms
+    },
+    secret: process.env.SECRET, 
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(
+      new PrismaClient(),
+      {
+        checkPeriod: 2 * 60 * 1000,  //ms
+        dbRecordIdIsSessionId: true,
+        dbRecordIdFunction: undefined,
+      }
+    )
+  })
+);
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
+app.use(flash())
 
 
 app.listen(process.env.HOST || 3000, (error) => {
---
+++ passport.js
@@ -0,0 +1,44 @@
+const passport = require('passport');
+const pool = require('./db/pool');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require("bcryptjs")
+
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
+
+passport.deserializeUser(async (id, done) => {
+    try{
+        const {rows} = await pool.query("SELECT * FROM User WHERE id = $1", [id])
+        const user = rows[0]
+        done(null, user)
+    } catch(err){
+        done(err)
+    }
+})
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const {rows} = await pool.query("SELECT * FROM User WHERE username = $1", [username])
+      const user = rows[0]
+      if(!user){
+        console.log("incorrect username")
+        return done(null, false, {message: "incorrect username"})
+      }
+      const match = await bcrypt.compare(password, user.password)
+      console.log(password)
+      console.log(user.password)
+      if(!match){
+        console.log("incorrect password")
+        return done(null, false, {message: "incorrect password"})
+      }
+      console.log("success")
+      return done(null, user)
+    } catch (err) {
+      return done(err)
+    }
+  })
+)
+
+module.exports = passport
