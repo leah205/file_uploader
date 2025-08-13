946e8f5e4daf073f65fb789f05758e6f017d350b
leah205 <leahtiktin@gmail.com>
2025-08-13 11:37:40 -0600

    feat: add errors to login

--- app.js
+++ app.js
@@ -8,10 +8,11 @@ const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
 const { PrismaClient } = require('./generated/prisma');
 const indexRouter = require('./routes/index')
 
-
+//make responsive
 const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use(
