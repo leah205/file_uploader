78f01158d73e987111ae9d94982cf4d1a9fba233
leah205 <leahtiktin@gmail.com>
2025-08-16 10:39:40 -0600

    feat: open folder modal when new folder button is pressed

--- public/main.js
+++ public/main.js
@@ -1,22 +1,35 @@
 const openUploadBtn = document.querySelector('.open-file-upload')
 const fileForm = document.querySelector('.file-form')
-const closeFileFormBtn = document.querySelector('.close-file-form')
+const closeModalBtns = document.querySelectorAll('.close-form')
 const modals = document.querySelectorAll('.modal')
 const fileModal = document.querySelector('.file-modal')
+const newFolderBtn = document.querySelector('.open-new-folder')
+const newFolderModal = document.querySelector('.new-folder-modal')
 openUploadBtn.addEventListener('click', ()=> {
     fileModal.style.display = 'flex'
 })
-
-closeFileFormBtn.addEventListener('click', () => {
-    fileModal.style.display='none'
+newFolderBtn.addEventListener('click', ()=> {
+    newFolderModal.style.display = 'flex'
 })
 
-modals.forEach((modal) => {
-     window.onclick = (event) => {
-      if (event.target === modal) {
+closeModalBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        const modal = btn.closest('.modal')
+        modal.style.display = 'none'
+    })
+})
+
+
+   
+    window.onclick = (event) => {
+       modals.forEach((modal) => {
+             if (event.target === modal) {
+         console.log(modal.classList)
         modal.style.display = "none";
       }
+       })
+     
     };
-})
+
 
  
