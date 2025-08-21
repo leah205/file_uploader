e011b7d0169d3429fe90eb8fe15145160660a360
leah205 <leahtiktin@gmail.com>
2025-08-20 19:58:19 -0600

    refactor: separate routes into distinct entities

--- routes/index.js
+++ routes/index.js
@@ -8,17 +8,23 @@ const authentication = require("../middleware/authentication.js")
 const folderController = require('../controllers/folderController.js')
 
 
-
+//user queries
 router.get('/signup', userController.signup.get)
 router.post('/signup', validation.signup, userController.signup.post)
 router.get('/login', userController.login.get)
 router.post('/login', userController.login.post)
-router.get('/', authentication.isAuth, homeController.index.get)
 router.get('/logout',userController.logout.get)
+
+//folder queries
+router.get('/', authentication.isAuth, homeController.index.get)
+router.post('/new-folder', folderController.newFolder.post)
+
+//file queries
 router.post('/file-upload', fileController.fileUpload.post)
 router.get('/file/:id', authentication.isAuth, fileController.fileDetails.get )
 router.post('/file/:id/delete', fileController.fileDetails.delete)
-router.post('/new-folder', folderController.newFolder.post)
+
+//router.get('/folder/:id', folderController.folder.get)
 
 
 
